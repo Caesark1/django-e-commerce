8a6011e0e6557aab340ff8e2995cfccbc0f24963
Caesark1 <kebec2015@gmail.com>
2021-03-17 20:41:51 +0600

    ajax fixed

--- static/js/main.js
+++ static/js/main.js
@@ -4,24 +4,23 @@ const categoriesInput = document.getElementById('categories')
 const featurenamesDataBox = document.getElementById('feature-names-data-box')
 const featurenamesInput = document.getElementById('feature-names')
 
-
-
 const categoryForm = document.getElementById('category-form')
-
 
 const categoryText = document.getElementById('category-text')
 
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
-var list_of_feature_id = []
+var list_of_feature_names = []
 
 $.ajax({
     type: 'GET',
     url: '/categories-json/',
-    success: function (response){
-        console.log(response.data)
+    success: function (response) {
+
+        console.log('success', response.data)
+
         const categoriesData = response.data;
-        categoriesData.map(item =>{
+        categoriesData.map(item => {
             const option = document.createElement('div');
             option.textContent = item.name
             option.setAttribute('class', 'item')
@@ -29,12 +28,12 @@ $.ajax({
             categoriesDataBox.appendChild(option)
         })
     },
-    error: function(error){
+    error: function (error) {
         console.log(error)
     }
 })
 
-categoriesInput.addEventListener('change', e=>{
+categoriesInput.addEventListener('change', e => {
     console.log(e.target.value)
     const selectedCategory = e.target.value
 
@@ -43,10 +42,12 @@ categoriesInput.addEventListener('change', e=>{
     $.ajax({
         type: "GET",
         url: `/feature-name-json/${selectedCategory}/`,
-        success: function(response){
+        success: function (response) {
             console.log(response.data)
             const featurenamesData = response.data
-            featurenamesData.map(item =>{
+            console.log('featurenamesData', featurenamesData)
+
+            featurenamesData.map(item => {
                 const option2 = document.createElement('label');
                 const option = document.createElement('input');
                 option2.textContent = item.feature_name
@@ -54,38 +55,53 @@ categoriesInput.addEventListener('change', e=>{
                 option.setAttribute('type', 'text')
                 option.setAttribute('name', item.feature_name)
                 option.setAttribute('placeholder', item.feature_name)
-                console.log(featurenamesDataBox)
+                option.setAttribute('id', item.id)
+                option.setAttribute('data-category-id', item.category_id)
+                list_of_feature_names.push(item.feature_name)
                 featurenamesDataBox.appendChild(option2)
                 featurenamesDataBox.appendChild(option)
+
+                // console.log('featurenamesDataBox', featurenamesDataBox)
             })
+
+
+
         },
-        error: function(error){
+        error: function (error) {
             console.log(error)
         }
     })
 
-})  
+})
 
 
-
-categoryForm.addEventListener('submit', e =>{
-    console.log(e.target.value)
+categoryForm.addEventListener('submit', e => {
     e.preventDefault()
-    console.log(e)
     console.log('submitted')
+
+    root = document.getElementById("feature-names-data-box")
+
+    post_items = []
+
+    items = root.getElementsByClassName('item')
+    for (index=0; index < items.length; index++){
+        post_items.push(items[index].value)
+    }
+
+
     $.ajax({
         type: "POST",
-        url:'/create/',
-        data:{
+        url: '/create/',
+        data: {
             'csrfmiddlewaretoken': csrf[0].value,
             'category': categoryText.textContent,
-            'feature_name': 'list_of_names',
+            'feature_names': list_of_feature_names.toString(),
+            'feature_values': post_items.toString(),
         },
-        success: function(response){
+        success: function (response) {
             console.log(response)
-            console.log(list_of_feature_id)
         },
-        error: function(error){
+        error: function (error) {
             console.log(error)
         }
 
